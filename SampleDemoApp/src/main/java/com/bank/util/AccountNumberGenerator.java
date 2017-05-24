c0e423259ca24f11f7e85083de144fcd03aef969
sagakulk <sagakulk@LIN51005151.corp.capgemini.com>
2017-05-24 15:42:52 +0530

    Moved variable declaration before constructor declaration in AccountNumberGenerator class

--- SampleDemoApp/src/main/java/com/bank/util/AccountNumberGenerator.java
+++ SampleDemoApp/src/main/java/com/bank/util/AccountNumberGenerator.java
@@ -2,8 +2,9 @@ package com.bank.util;
 
 public class AccountNumberGenerator {
 
-	private AccountNumberGenerator(){}
 	private static int accountCounter = 1;
+	private AccountNumberGenerator(){}
+	
 
 	public static int getNewAccountNumber() {
 		return accountCounter++;
